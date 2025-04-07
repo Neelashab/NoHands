20e19528bfbe4f6d96be3ea034278232ab0f48f8
katherineoung <katherine.oung@gmail.com>
2025-04-07 17:24:15 -0500

    add pure pursuit controller functions

--- NoHandsAvStack/src/example_project.jl
+++ NoHandsAvStack/src/example_project.jl
@@ -146,6 +146,18 @@ function get_route(map, my_location)
     [24,17,14,10,84,82,80]
 end
 
+# --- NEW CODE HERE ----- #
+
+# --- Polyline Calculations --- #
+# how to make it work for the input and curved line segments?
+
+abstract type PolylineSegment end # defines a type called a polyline segment
+# the structs are the implementations of this type
+
+function perp(x)
+    [-x[2], x[1]]
+end
+
 struct StandardSegment <: PolylineSegment
     p1::SVector{2, Float64}
     p2::SVector{2, Float64}
@@ -159,7 +171,39 @@ struct StandardSegment <: PolylineSegment
     end
 end
 
-#TO DO: remove starting and terminal rays
+# curved segment?
+struct CurvedSegment <: PolylineSegment
+    p1::SVector{2, Float64}
+    p2::SVector{2, Float64}
+    tangent::SVector{2, Float64}
+    normal::SVector{2, Float64}
+    curvature::Float64  # 1 / radius of curvature
+
+    function CurvedSegment(p1, p2, curvature::Float64)
+        # Compute the tangent vector
+        tangent = p2 - p1
+        tangent ./= norm(tangent)
+        
+        # The normal vector is perpendicular to the tangent
+        normal = perp(tangent)
+        
+        # Adjust the segment based on curvature
+        # The curvature adjusts how much we deviate from the straight line.
+        # A positive curvature will make the segment curve in one direction, negative for the other.
+        
+        # The offset distance for curvature: offset = curvature * distance
+        distance = norm(p2 - p1)
+        offset = curvature * distance  # Larger curvature -> larger offset
+        
+        # Move the p2 point based on the curvature
+        p2_curved = p2 + offset * normal
+        
+        # Return the curved segment
+        new(p1, p2_curved, tangent, normal, curvature)
+    end
+end
+
+# remove starting and terminal rays
 struct Polyline
     segments::Vector{PolylineSegment} # array-like collection of segments
     function Polyline(points)
@@ -170,7 +214,6 @@ struct Polyline
             seg = StandardSegment(points[i], points[i+1]) 
             push!(segments, seg)
         end
-        push!(segments, terminal_ray)
         new(segments) # segments is an array of the points, tan, and norm of each segment
     end
     function Polyline(points...)
@@ -183,6 +226,165 @@ function get_polyline(map, my_location, target_segment)
     points = [get_center(route[i], map, 80) for r =1:length(route)]
     Polyline(points)
 end
+
+"""
+compute the signed distance from POINT to POLYLINE. 
+
+Note that point has a positive signed distance if it is in the same side of the polyline as the normal vectors point.
+"""
+# compute signed part of signed distance
+function signDist(a, b)
+    dot_prod = dot(a, b)
+    if dot_prod > 0
+        return 1.0
+    elseif dot_prod < 0
+        return -1.0
+    else
+        return 1.0 
+    end
+end
+
+# negative if to the right of normal vector, positive if to the left
+
+# use binary search method to find alpha to minimize load time
+
+# compute signed distance from standard
+function signed_distance_standard(seg::StandardSegment, q) 
+    # alpha is an interpolating value - find the alpha value that results in the minimum distance
+
+    # dist should be the minimum 
+    alpha0 = 0.0
+    alpha1 = 1.0
+    dist0 = norm(alpha0*seg.p1 + (1.0 - alpha0)*seg.p2 - q)
+    dist1 = norm(alpha1*seg.p1 + (1.0 - alpha1)*seg.p2 - q)
+    
+    while abs(alpha1 - alpha0) > 0.00000001 || abs(dist0-dist1) > 0.0000000001
+        alpha = (alpha0+alpha1)/2.0
+        new_point = alpha*seg.p1 + (1.0 - alpha)*seg.p2
+        diff = new_point - q
+        dist = norm(alpha*seg.p1 + (1.0 - alpha)*seg.p2 - q)
+        
+        if abs(dist1) < abs(dist0)
+            dist0 = dist
+            alpha0 = alpha
+        elseif abs(dist1) > abs(dist0)
+            dist1 = dist
+            alpha1 = alpha
+        else
+            break
+        end
+    end
+
+    return signDist(seg.normal, q - seg.p1)*dist0
+
+end
+
+# loop through computations to find the minimum a
+function signed_distance(polyline::Polyline, point)
+    N = length(polyline.segments)
+
+    dist_min = starting_dist; # find min starting w starting ray
+
+    for i = 2:(N -1) # do you start at the second point?
+        dist = signed_distance_standard(polyline.segments[i], point)
+        if abs(dist) < abs(dist_min)
+            dist_min = dist
+        end
+    end
+
+    if abs(term_dist) < abs(dist_min)
+        dist_min = term_dist
+    end
+    return dist_min
+
+    return 0.0
+end
+
+# -- CONTROL -- #
+
+function control(state_ch, control_ch, stop_ch, path, L)
+    pure_pursuit(state_ch, control_ch, stop_ch, path, L)
+    # or try dummy_controller(...) if you like
+end
+
+# signed distance function
+function signOfDot(a, b)
+    dot_prod = dot(a, b)
+    if dot_prod > 0
+        return 1.0
+    elseif dot_prod < 0
+        return -1.0
+    else
+        return 0.0
+    end
+end
+
+# should car steer left or right?
+# calculate the normal vector to the vehicle's direction vector.
+# rotate 90 degrees counterclockwise
+# sign of Dot tells you if you're to the left or right
+# For a 2D vector [x, y], the normal vector is [-y,x]
+function left_right(v_direction, target_direction)
+    v_normal = [-v_direction[2], v_direction[1]]
+    return signOfDot(v_normal, target_direction)
+end
+
+function pure_pursuit(state_ch, control_ch, stop_ch, path, L; ls = 1.445) # ls = lookahead time
+    N = length(path.segments)
+
+    while true
+        sleep(0.01)
+        fetch(stop_ch) && return # check if this task should end
+        x = fetch(state_ch) # get latest simulation state
+        p1, p2, θ, v = x # unpack state variables
+        
+# ------- my control law implementation ---
+        vehicle_pos = [p1, p2]
+        len0 = v * ls # velocity times look ahead time
+        min_diff = len0 # difference between desired and durr distance - like alpha
+        best_i = 0 # next best target on path
+        v_direction = [cos(θ), sin(θ)] # direction vector of heading based on its orientation angle θ - like delta
+
+        # for all the standard segments
+        # find the segment that is the minimum distance away
+        for i = 2 : N - 1
+            target = path.segments[i].p1 # try current path segment as target
+            sign = signOfDot(v_direction, target - vehicle_pos) 
+            if sign > 0 # target pos must be ahead of vehicle
+                l = norm(target - vehicle_pos) 
+                diff = abs(l - len0) #euc distance -> absolute distance
+                if diff < min_diff 
+                    min_diff = diff
+                    best_i = i
+                end
+            end
+        end
+        
+        if best_i > 0
+            target = path.segments[best_i].p1
+            # cosine of the angle between the vehicle's direction and the target direction
+            cos_alpha = dot(v_direction, target - vehicle_pos) / norm(target - vehicle_pos)
+            # calculate the angle alpha using the arccosine function
+            alpha = acos(cos_alpha)
+            # calculate the sine of the angle alpha
+            sin_alpha = sin(alpha)
+            left_or_right = left_right(v_direction, target - vehicle_pos)
+            # calculate the steering angle (delta) using a formula
+            delta = atan(2.0 * L * sin_alpha * left_or_right, v * ls)
+            u = [delta, 0.0]
+            println("line 88")
+        else
+            println("line 90")
+            u = zeros(2) # No valid target found
+        end
+# ------- my control law implementation ---
+
+        take!(control_ch)  # Clear old control
+        put!(control_ch, u)  # Put new control on the channel
+    end
+end
+
+# --- NEW CODE ENDS ----- #
 
 function decision_making(localization_state_channel, 
         perception_state_channel, 
@@ -204,7 +406,7 @@ function decision_making(localization_state_channel,
             latest_localization_state = fetch(localization_state_channel)
             println("dm func state: $latest_localization_state")
             #latest_perception_state = fetch(perception_state_channel)
-            # figure out what to do ... setup motion planning problem etc
+            # TO-DO: change these functions to call the code written
             steering_angle = 0.0
             target_vel = 1.0
             cmd = (steering_angle, target_vel, true)
