3b5ae94a54afb72f3cbf076ca5e762a668908472
rachelmhawkins <143203688+rachelmhawkins@users.noreply.github.com>
2025-04-08 05:22:34 -0500

    add req packages

--- NoHandsAvStack/src/NoHandsAvStack.jl
+++ NoHandsAvStack/src/NoHandsAvStack.jl
@@ -1,8 +1,23 @@
 module NoHandsAvStack
 
-using VehicleSim
+using Statistics
+using ColorTypes
+using Dates
+using GeometryBasics
+using Graphs
+using MeshCat
+using MeshCatMechanisms
+using Random
+using Rotations
+using RigidBodyDynamics
+using Infiltrator
+using LinearAlgebra
+using Printf
+using SparseArrays
+using Suppressor
 using Sockets
 using Serialization
+using StaticArray
 
 include("client.jl")
 include("example_project.jl")
