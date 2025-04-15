94816be53eb2bf245c691b74c82e2541921e97cf
Neelasha <neelashab@gmail.com>
2025-04-15 17:03:16 -0500

    added client logic

--- NoHandsAvStack/src/NoHandsFinal.jl
+++ NoHandsAvStack/src/NoHandsFinal.jl
@@ -475,3 +475,136 @@ function ekf(track::TrackedObject, z::SVector{3, Float64}, Δt::Float64)
     
     return ekf_updated_track
 end
+
+
+
+function my_client(host::IPAddr=IPv4(0), port=4444; use_gt=true)
+    socket = Sockets.connect(host, port)
+    map_segments = VehicleSim.city_map()
+    
+    msg = deserialize(socket) # Visualization info
+    @info msg
+
+    gps_channel = Channel{GPSMeasurement}(32)
+    imu_channel = Channel{IMUMeasurement}(32)
+    cam_channel = Channel{CameraMeasurement}(32)
+    gt_channel = Channel{GroundTruthMeasurement}(32)
+
+    localization_state_channel = Channel{MyLocalizationType}(1)
+    perception_state_channel = Channel{MyPerceptionType}(1)
+    target_segment_channel = Channel{Int}(1)
+    ego_vehicle_id_channel = Channel{Int}(1)
+    avoid_collision_channel = Channel{Float64}(1)
+    put!(avoid_collision_channel, 10)#speed limit
+    shutdown_channel = Channel{Bool}(1)
+    put!(shutdown_channel, false)
+
+    target_map_segment = 0 # (not a valid segment, will be overwritten by message)
+    ego_vehicle_id = 0 # (not a valid id, will be overwritten by message. This is used for discerning ground-truth messages)
+
+    put!(target_segment_channel, target_map_segment)
+    put!(ego_vehicle_id_channel, ego_vehicle_id)
+    #println("before errormonitor(@async while true")
+    errormonitor(@async while true
+        # This while loop reads to the end of the socket stream (makes sure you
+        # are looking at the latest messages)
+        sleep(0.001)
+        local measurement_msg
+        received = false
+        while true
+            @async eof(socket)
+            if bytesavailable(socket) > 0
+                #println("bytesavailble")
+                measurement_msg = deserialize(socket)
+                received = true
+            else
+                #println("no more bytesavailble")
+                break
+            end
+        end
+        !received && continue
+        target_map_segment = measurement_msg.target_segment
+        old_target_segment = fetch(target_segment_channel)
+        if target_map_segment ≠ old_target_segment
+            take!(target_segment_channel)
+            put!(target_segment_channel, target_map_segment)
+        end
+        ego_vehicle_id = measurement_msg.vehicle_id
+        old_ego_vehicle_id = fetch(ego_vehicle_id_channel)
+        if ego_vehicle_id ≠ old_ego_vehicle_id
+            take!(ego_vehicle_id_channel)
+            put!(ego_vehicle_id_channel, ego_vehicle_id)
+        end
+
+        for meas in measurement_msg.measurements
+            #println("for meas in measurement_msg.measurements")
+            if meas isa GPSMeasurement
+                #println("meas isa GPSMeasurement")
+                !isfull(gps_channel) && put!(gps_channel, meas)
+            elseif meas isa IMUMeasurement
+                #println("meas isa IMUMeasurement")
+                !isfull(imu_channel) && put!(imu_channel, meas)
+            elseif meas isa CameraMeasurement
+                #println("meas isa CameraMeasurement")
+                !isfull(cam_channel) && put!(cam_channel, meas)
+            elseif meas isa GroundTruthMeasurement
+                #println("meas isa GroundTruthMeasurement")
+                !isfull(gt_channel) && put!(gt_channel, meas)
+            end
+        end
+    end)
+    
+    if use_gt
+        errormonitor(@async process_gt(gt_channel,
+                      shutdown_channel,
+                      localization_state_channel,
+                      perception_state_channel, 
+                      ego_vehicle_id_channel))
+    else
+        errormonitor(@async localize(gps_channel, 
+                    imu_channel, 
+                    localization_state_channel, 
+                    shutdown_channel))
+
+        errormonitor(@async perception(cam_channel, 
+                      localization_state_channel, 
+                      perception_state_channel, 
+                      shutdown_channel))
+    end
+
+    errormonitor(@async avoid_collision(localization_state_channel, 
+                           perception_state_channel,
+                           avoid_collision_channel,
+                           shutdown_channel
+                           ))
+
+    errormonitor(@async decision_making(localization_state_channel, 
+                           perception_state_channel, 
+                           target_segment_channel, 
+                           avoid_collision_channel,
+                           shutdown_channel,
+                           map_segments, 
+                           socket))
+end
+
+function shutdown_listener(shutdown_channel)
+    info_string = 
+        "***************
+      CLIENT COMMANDS
+      ***************
+            -Make sure focus is on this terminal window. Then:
+            -Press 'q' to shutdown threads. 
+    "
+    @info info_string
+    while true
+        sleep(0.1)
+        key = get_c()
+
+        if key == 'q'
+            # terminate threads
+            take!(shutdown_channel)
+            put!(shutdown_channel, true)
+            break
+        end
+    end
+end
